3cf5df15acb5f343b9521d0b06d595fba5063db8
Aounhassan275 <aounhassan275@gmail.com>
2023-05-31 02:15:16 +0500

    fix

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -54,6 +54,37 @@
             </div>
         </a>
     </div>
+    <div class="col-sm-4 col-xl-4">
+        <a href="{{route('admin.respondent_master.index')}}">
+            <div class="card card-body bg-orange-400 has-bg-image">
+                <div class="media">
+                    <div class="media-body">
+                    <h3 class="mb-0">{{App\Models\RespondentMaster::count()}}</h3>
+                        <span class="text-uppercase font-size-xs">Total Respondent Master</span>
+                    </div>
+
+                    <div class="ml-3 align-self-center">
+                        <i class="icon-blog icon-3x opacity-75"></i>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-sm-4 col-xl-4">
+        <a href="{{route('admin.farming_profile.index')}}">
+            <div class="card card-body bg-danger-400 has-bg-image">
+                <div class="media">
+                    <div class="mr-3 align-self-center">
+                        <i class="icon-stack-picture icon-3x opacity-75"></i>
+                    </div>
+                    <div class="media-body text-right">
+                    <h3 class="mb-0">{{App\Models\FarmingProfile::count()}}</h3>
+                        <span class="text-uppercase font-size-xs">Total Farming Profile</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
 </div>
 @endsection
 @section('scripts')
