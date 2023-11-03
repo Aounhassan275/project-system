a161619d51a3115bedb0bd2a73b6689ccc7fb00c
srdcpvtltd <debendraster@gmail.com>
2023-11-03 22:38:18 +0530

    Update index.blade.php

--- resources/views/executive/dashboard/index.blade.php
+++ resources/views/executive/dashboard/index.blade.php
@@ -53,6 +53,21 @@
             </div>
         </a>
     </div>
+    <div class="col-sm-4 col-xl-4">
+        <a href="#">
+            <div class="card card-body bg-green-400 has-bg-image">
+                <div class="media">
+                    <div class="mr-3 align-self-center">
+                        <i class="icon-stack-picture icon-3x opacity-75"></i>
+                    </div>
+                    <div class="media-body text-right">
+                    <h3 class="mb-0">{{Auth::user()->projects->count()}}</h3>
+                        <span class="text-uppercase font-size-xs">Trainings</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
 </div>
 @endsection
 @section('scripts')
