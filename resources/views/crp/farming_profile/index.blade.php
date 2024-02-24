305a00fdcd5d5d49133f9af7ff7034dabb4b5203
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-02-24 23:01:31 +0530

    Update index.blade.php

--- resources/views/crp/farming_profile/index.blade.php
+++ resources/views/crp/farming_profile/index.blade.php
@@ -25,22 +25,19 @@
                         <th>SL No</th>
                         <th>Farmer ID</th>
                         <th>Farmer Name</th>
-                        <th>CRP</th>
                         <th>Date & Time of Update</th>
-                        <th>Validated</th>
-                        <th>Action</th>
-                        <th>Action</th>
+                    <th>Is Validate</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach (Auth::user()->farmingProfiles  as $key => $farming_profile)
                 <tr>
-                         <td>{{$key+1}}</td>
-                        <td>{{@$farming_profile->respondent_master->farmer_id}}</td>
-                        <td>{{@$farming_profile->respondent_master->name}}</td>
-                        <td>{{@$farming_profile->user->name}}</td>
-                        <td>{{@$farming_profile->updated_at}}</td>
-                        <td>
+                    <td>{{$key+1}}</td>
+                    <td>{{@$farming_profile->respondent_master->farmer_id}}</td>
+                    <td>{{@$farming_profile->respondent_master->name .'('.@$farming_profile->respondent_master->farmer_id.')'}}</td>
+                    <td>{{@$farming_profile->updated_at}}</td>
+                    <td>
                         @if($farming_profile->is_validate)
                         <span class="badge badge-success">Yes</span>
                         @else 
